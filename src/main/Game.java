764a5470ec6cb179c80572e5e817bfd1025a41e9
KyNaOo <kevan122.wu@gmail.com>
2023-11-08 10:45:19 +0100

    feat:map import

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,22 +1,46 @@
 package src.main;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
 public class Game extends JFrame {
 
     private GameScreen gameScreen;
 
+    private BufferedImage img;
+
     public Game() {
+
+        importImg();
+
         setSize(640, 640);
-        setVisible(true);
+        
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Use JFrame.EXIT_ON_CLOSE
         setLocationRelativeTo(null);
 
-        gameScreen=new GameScreen();
+        gameScreen=new GameScreen(img);
         add(gameScreen);
+        setVisible(true);
+    }
+
+   
+     private void importImg() {
+        InputStream is = getClass().getResourceAsStream("img/spriteatlas.png");
+
+        try {
+            img = ImageIO.read(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
         Game game = new Game();
     }
+
+
 }
--- src/main/GameScreen.java
+++ src/main/GameScreen.java
@@ -4,23 +4,60 @@ import javax.swing.JPanel;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class GameScreen extends JPanel{
 
     private Random random;
+    private BufferedImage img;
+    private ArrayList<BufferedImage> sprites = new ArrayList<>();
 
-    public GameScreen(){
+    public GameScreen(BufferedImage img){
+        this.img=img;
+        loadSprites();
         random=new Random();
     }
+
+    private void loadSprites() {
+        for (int y = 0; y < 3; y++) {
+            for(int x = 0; x < 10; x++) {
+                sprites.add(img.getSubimage(x*32, y*32, 32, 32));
+            }
+
+    }
+}
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         
-        for(int x = 0; x<20;x++){
-            //g.setColor(Color .RED); Basic color
-            g.setColor(new Color(105,10,50));//Custom color
-            g.fillRect(x*32, 0, 32, 32);
+        for (int y=0; y<20; y++) {
+            for(int x=0; x<20;x++){
+                g.drawImage(sprites.get(getRndInt()), x*32, y*32, null);
+            }
         }
+        
+
+
+        /*for(int x = 0; x<20;x++){
+            for(int y=0; y<20;y++){
+            //g.setColor(Color .RED); Basic color
+            g.setColor(getRndColor());//Custom color
+            g.fillRect(x*32, y*32, 32, 32);
+            }
+        }*/  
+    }
+
+    private int getRndInt(){
+        return random.nextInt(29);
+    }
+
+    private Color getRndColor(){
+        int r= random.nextInt(256);
+        int g=  random.nextInt(256);
+        int b=  random.nextInt(256);
+
+        return new Color(r,g,b);
     }
 }
