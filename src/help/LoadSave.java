e54ce654c6e63b5528c466729aca6cb7deb4036a
Ethanol <ethanbellaiche0@gmail.com>
2023-11-10 15:27:52 +0100

    feat: path for sprites

--- src/help/LoadSave.java
+++ src/help/LoadSave.java
@@ -11,7 +11,7 @@ public class LoadSave {
 	public static BufferedImage getSpriteAtlas() {
 
 		BufferedImage img = null;
-		InputStream is = LoadSave.class.getClassLoader().getResourceAsStream("src/help/spriteatlas.png");
+		InputStream is = LoadSave.class.getClassLoader().getResourceAsStream("src/help/newsprite.png");
 
 		try {
 			img = ImageIO.read(is);
